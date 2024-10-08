2e6066975563a050be9955dc4296afa5deaecf51
milkioiy <93613157+milkioiy@users.noreply.github.com>
2024-10-08 23:32:41 +0900

    Update BJ2003.java

--- src/main/java/com/example/codingtest_1/BJ2003.java
+++ src/main/java/com/example/codingtest_1/BJ2003.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class bjnumbersum2 {
+public class BJ2003 {
 
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
